259ac2446586a92b867f978a480ce3158fc5ae50
Gabriel <gabriel.gsm2017@gmail.com>
2022-05-02 12:48:48 -0300

    adicionando os anos

--- src/Components/Selects/index.jsx
+++ src/Components/Selects/index.jsx
@@ -8,6 +8,8 @@ function Selects() {
   const [selectedBrand, setSelectedBrand] = useState(null);
   const [models, setModels] = useState([]);
   const [selectedModel, setSelectedModel] = useState(null);
+  const [years, setYears] = useState([]);
+  const [yearsSelected, setYearsSelected] = useState(null);
   
   useEffect(()=> {
     axios.get(`${apiBase}/carros/marcas`).then((response) =>{
@@ -32,26 +34,34 @@ function Selects() {
           const getModels = response.data.modelos.map(e => {
             return { value: e.codigo, label: e.nome };
           })
-          setModels(getModels)
+          setModels(getModels);
+          
+          const getYears = response.data.anos.map(e => {
+            return { value: e.codigo, label: e.nome };
+          })
+          setYears(getYears);
         }
       })
     }
   },[selectedBrand]);
-
+  
   const modelChange = obj => {
     setSelectedModel(obj);
-
   }
   useEffect(() => {
-    if(selectedModel){
-      axios.get(`${apiBase}/carros/marcas/${selectedBrand?.value}/modelos/${selectedModel?.value}/anos`).then((response) =>{
+    if(selectedModel) {
+      axios.get(`${apiBase}/carros/marcas/${selectedBrand.value}/modelos/${selectedModel.value}/anos`).then((response) => {
         if(response.data){
-          console.log(response.data)      
+          const getYears = response.data.map(e => {
+            return { value: e.codigo, label: e.nome };
+          });
+          console.log({getYears})
+          setYears(getYears)
         }
-      })
+      });
     }
-  },[selectedModel]);
-  
+  }, [selectedModel]);
+
   
   return (
     <div>
@@ -71,6 +81,7 @@ function Selects() {
       <Select
         className='select' 
         placeholder='Buscar Ano'
+        options={years}
       />
     </div>    
   )
