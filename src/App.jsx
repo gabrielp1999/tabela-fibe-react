3939411611d1e98bdddac25145929390433e359d
Gabriel <gabriel.gsm2017@gmail.com>
2022-05-04 15:02:01 -0300

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Main from './Components/Main';
 
 
 function App() {
+  document.title = 'Tabela Fipe';
   return (
     <div className="App">
+
       <Main />
     </div>
   );
--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,51 +1,66 @@
 import React from 'react';
 import './Details.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function Details(props) {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
   if(!props.show) {
     return null;
   }
 
   return (
-    <div className='details'>
 
-        <div className='div-icon'>
-          <div className='icon-left' ><img alt='' src='https://cdn-icons-png.flaticon.com/128/54/54227.png'/></div>
-          <div className='icon-right' ><img alt='' src='https://cdn-icons-png.flaticon.com/128/54/54366.png' /></div>
+    
+      <div className='details'>
+         <Slider className='sliderBiel' {...settings}>
+        <div className='div-info'>
+          <div className='card'>
+            <div className='key'>Mês de referência:</div>
+            <div className='value'>{props.car.MesReferencia}</div>
+          </div>
+          <div className='card'>
+            <div className='key'>Marca:</div>
+            <div className='value'>{props.car.Marca}</div>
+          </div>
+          <div className='card'>
+            <div className='key'>Modelo:</div>
+            <div className='value'>{props.car.Modelo}</div>
+          </div>
+          <div className='card'>
+            <div className='key'>Ano do modelo:</div>
+            <div className='value'>{props.car.AnoModelo}</div>
+          </div>
+          <div className='card'>
+            <div className='key'>Combustivel:</div>
+            <div className='value'>{props.car.Combustivel}</div>
+          </div>
+          <div className='card'>
+            <div className='key'>Codigo fipe:</div>
+            <div className='value'>{props.car.CodigoFipe}</div>
+          </div>
+          <div className='card card-value-car'>
+            <div className='key'>Preço medio:</div>
+            <div className='value'>{props.car.Valor}</div>
+          </div>
+        </div> 
+        <div className='div-img'>
+          <img src="http://1.bp.blogspot.com/-RtAyYJ-wDMI/UqHHITQj9dI/AAAAAAAAF5s/-MZKETwfxn0/s1600/carro_top2.png" alt="" />
         </div>
-  
-      <div className='div-info'>
-        <div className='card'>
-          <div className='key'>Mês de referência:</div>
-          <div className='value'>{props.car.MesReferencia}</div>
-        </div>
-        <div className='card'>
-          <div className='key'>Marca:</div>
-          <div className='value'>{props.car.Marca}</div>
-        </div>
-        <div className='card'>
-          <div className='key'>Modelo:</div>
-          <div className='value'>{props.car.Modelo}</div>
-        </div>
-        <div className='card'>
-          <div className='key'>Ano do modelo:</div>
-          <div className='value'>{props.car.AnoModelo}</div>
-        </div>
-        <div className='card'>
-          <div className='key'>Combustivel:</div>
-          <div className='value'>{props.car.Combustivel}</div>
-        </div>
-        <div className='card'>
-          <div className='key'>Codigo fipe:</div>
-          <div className='value'>{props.car.CodigoFipe}</div>
-        </div>
-        <div className='card card-value-car'>
-          <div className='key'>Preço medio:</div>
-          <div className='value'>{props.car.Valor}</div>
-        </div>
-    </div> 
+
+      </Slider>
     </div>
+
   )
 
 }
